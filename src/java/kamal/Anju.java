4147ecb34c9ae8b667fab6b551ddd958a5b5bac3
Gilpix <kuldeepsingh5005@gmail.com>
2019-10-16 10:14:13 -0400

    getSingleJob details created in Anju class

--- src/java/kamal/Anju.java
+++ src/java/kamal/Anju.java
@@ -8,6 +8,7 @@ package kamal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
@@ -16,6 +17,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -27,8 +29,10 @@ import net.sf.json.JSONObject;
  */
 @Path("jobs")
 public class Anju {
+    
+    
      
- String status; 
+ String msg; 
  Date today=new Date();
  long echoTime=today.getTime();
  JSONArray mainArray=new JSONArray();
@@ -118,29 +122,98 @@ public class Anju {
     }
      mainObject.accumulate("status", "ok");
         mainObject.accumulate("Timestamp", echoTime);
-    mainObject.accumulate("Countries", mainArray);
+    mainObject.accumulate("Jobs", mainArray);
     
     
      kul.closeConnection(conn,rs,stm);
             
         } catch (Exception ex) {
             //Logger.getLogger(Kuldeeep.class.getName()).log(Level.SEVERE, null, ex);
-            status=ex.getMessage();
+            msg=ex.getMessage();
         }
         
-        if(mainObject.get("Countries").toString().equals("[]"))
+        if(mainObject.get("Jobs").toString().equals("[]"))
         {
             mainObject.clear();
                    
              mainObject.accumulate("Status", "error");
         mainObject.accumulate("Timestamp", echoTime);
-        mainObject.accumulate("Msg",  status);
+        mainObject.accumulate("Msg",  msg);
        }
        
  
          return mainObject.toString();
          
     }
+    
+    
+    
+    
+    
+    
+      @GET
+            @Path("getSingleJob&{j_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getSingleJob(@PathParam("j_id") String j_id) {
+        
+        
+        JSONObject singleJob =new JSONObject();
+        mainObject.clear();
+        mainArray.clear();
+        
+        
+        Connection conn = null;
+              conn=  kul.getConnection(conn);
+         
+        try {          
+            
+            
+            
+              String sql;
+    sql = "SELECT JOB_ID,JOB_TITLE,MIN_SALARY,MAX_SALARY FROM JOBS where JOB_ID=?";
+    
+   
+   
+      PreparedStatement stm = conn.prepareStatement(sql);
+                stm.setString(1,j_id);
+
+                ResultSet rs=stm.executeQuery();
+
+                while(rs.next()) {
+     String job_id = rs.getString("JOB_ID");
+    String jTitle = rs.getString("JOB_TITLE");
+    int min_sal = rs.getInt("MIN_SALARY");
+     int max_sal = rs.getInt("MAX_SALARY");
+//Display values
+ singleJob.accumulate("JOB_ID", j_id);
+        singleJob.accumulate("JOB_TITLE", jTitle);
+          singleJob.accumulate("MIN_SALARY", min_sal);
+          singleJob.accumulate("MAX_SALARY", max_sal);
+     
+    }
+
+        }
+        catch (SQLException ex) {
+                       msg=ex.getMessage();
+                      
+                    } catch (Exception ex) {
+              msg=ex.getMessage();
+          }
+ 
+        if(singleJob.toString().equals("{}"))
+        {
+              if(msg==null)
+               msg="Record not found";
+                   
+             singleJob.accumulate("Status", "error");
+        singleJob.accumulate("Timestamp", echoTime);
+        singleJob.accumulate("Msg",  msg);
+       }
+        
+         return singleJob.toString();
+         
+    }
+    
     
     
     
